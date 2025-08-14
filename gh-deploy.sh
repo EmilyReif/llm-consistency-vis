f294dd81f0f747113ccbfb07daece543bfe6a017
Emily Reif <emilyreif@gmail.com>
2025-08-14 10:57:03 -0700

    updates to gh pages workflow

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
-git subtree push --prefix $1 origin gh-pages
+set -e
+
+npm run build
+cp build/index.html build/404.html
+
+# Create/checkout gh-pages branch in a temp worktree
+rm -rf .gh-pages-tmp
+git worktree add .gh-pages-tmp gh-pages
+
+# Copy build output into the gh-pages worktree
+rm -rf .gh-pages-tmp/*
+cp -r build/* .gh-pages-tmp/
+
+# Commit and push
+cd .gh-pages-tmp
+git add --all
+git commit -m "Deploy $(date)"
+git push origin gh-pages
+
+# Clean up
+cd ..
+git worktree remove .gh-pages-tmp
+rm -rf build
