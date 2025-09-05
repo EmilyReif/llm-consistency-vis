1c88654ac9f5e27cc1f61684d5f732313f808175
Emily Reif <emilyreif@gmail.com>
2025-09-05 14:34:40 -0700

    Adding hacky olmo trace data

--- src/cached_examples.tsx
+++ src/cached_examples.tsx
@@ -1,5 +1,6 @@
 import { examplesOrig } from "./cached_data/examples";
 import { examplesNoveltyBench } from "./cached_data/novelty_bench";
 import { examplesQalign } from "./cached_data/qalign";
+import { exampleOlmoTrace } from "./cached_data/examples_olmo_trace";
 
 export const examples = examplesOrig;
